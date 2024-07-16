94f7cb459408cd93f62c07359192d838eac3f5b8
Cassandra Keddis <cassandra@thinkific.com>
2024-07-16 17:45:08 -0500

    feat(actions): edit blocks action

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -20,9 +20,14 @@ export async function createBlock(formData: FormData) {
 //   redirect("/blocks");
 // }
 
-// async function editBlock(formData: FormData) {
-//   const title = formData.get("title") as string;
-//   const code = formData.get("code") as string;
-//   await db.block.create({ data: { title, code } });
-//   redirect("/blocks");
-// }
+export async function editBlock(id: number, formData: FormData) {
+  const title = formData.get("title") as string;
+  const code = formData.get("code") as string;
+
+  await db.block.update({
+    where: { id },
+    data: { title, code },
+  });
+
+  redirect(`/blocks/${id}`);
+}
